5f5c35270e3680f2361f626e7fe0390477dfbafb
IImRan007 <mohd78696321@gmail.com>
2023-12-16 18:43:16 +0530

    added backend url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://chat-app-with-chat-gpt-backend.vercel.app",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
