eb0eed51954074ed79fcb931a8be7238499c8435
IImRan007 <mohd78696321@gmail.com>
2023-12-16 18:37:59 +0530

    added backend url

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,12 @@ import App from "./App.jsx";
 import "./index.css";
 // Context
 import { UserProvider } from "./context/user/UserContext.jsx";
-import { ProductProvider } from "./context/product/ProductContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <UserProvider>
-      <ProductProvider>
-        <App />
-      </ProductProvider>
+      <App />
     </UserProvider>
   </BrowserRouter>
 );
